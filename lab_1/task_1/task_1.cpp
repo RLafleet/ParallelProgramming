d6ae7b48d530567fd3d78493801b0e53898804a9
RLafleet <maksim.nazarov@ispring.institute>
2024-09-19 18:07:07 +0300

    Add project files.

---
+++ lab_1/task_1/task_1.cpp
@@ -0,0 +1,46 @@
+﻿#include <windows.h>
+#include <string>
+#include <iostream>
+#include <sstream>
+
+DWORD WINAPI ThreadProc(LPVOID lpParam)
+{
+	int threadNumber = (int)lpParam;
+	std::string output = "The flow number " + std::to_string(threadNumber) + " is running\n";
+	std::cout << output;
+
+	ExitThread(0); 
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc < 2)
+	{
+		std::cout << "Invalid argument count" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	std::istringstream ss(argv[1]);
+	int n;
+
+	if (!(ss >> n))
+	{
+		std::cout << "Invalid number passed" << std::endl;
+	}
+
+	HANDLE* handles = new HANDLE[n];
+
+	for (int i = 1; i <= n; i++)
+	{
+		handles[i] = CreateThread(NULL, 0, &ThreadProc, reinterpret_cast<LPVOID>(i), CREATE_SUSPENDED, NULL);
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		ResumeThread(handles[i]);
+	}
+
+	WaitForMultipleObjects(n, handles, true, INFINITE);
+
+	return EXIT_SUCCESS;
+}
