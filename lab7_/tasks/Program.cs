391c9a3d96bc7edfb235d0213de85823e9c21361
RLafleet <maksim.nazarov@ispring.institute>
2024-12-25 20:56:36 +0300

    t_2

---
+++ lab7_/tasks/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace lab7;
+
+internal static class Program
+{
+    private static async Task Main()
+    {
+        Console.Write("Enter the path to the text file: ");
+        var filePath = Console.ReadLine();
+
+        Console.Write("Enter characters to remove (without spaces): ");
+        var charsToRemove = Console.ReadLine();
+
+        try
+        {
+            var content = await File.ReadAllTextAsync(filePath ?? "");
+
+            var result = new string(content.Where(c => !charsToRemove?.Contains(c) ?? true).ToArray());
+
+            await File.WriteAllTextAsync(filePath ?? "", result);
+
+            Console.WriteLine("Characters successfully removed and file saved.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+}
